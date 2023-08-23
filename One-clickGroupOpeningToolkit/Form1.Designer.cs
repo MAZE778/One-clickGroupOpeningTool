6ffb0a93ee7ab81e10318be25a2c7f7c84f3e2fe
MAZE778 <qinzihang20091217@163.com>
2023-08-23 19:46:02 +0800

    添加项目文件。

---
+++ One-clickGroupOpeningToolkit/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace One_clickGroupOpeningToolkit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mianzeshengming = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.fangfa = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kaituan = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.num5 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(119, 16);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(73, 25);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label1.Location = new System.Drawing.Point(16, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "发送次数:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "条";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // mianzeshengming
+            // 
+            this.mianzeshengming.AutoSize = true;
+            this.mianzeshengming.Location = new System.Drawing.Point(367, 46);
+            this.mianzeshengming.Name = "mianzeshengming";
+            this.mianzeshengming.Size = new System.Drawing.Size(15, 14);
+            this.mianzeshengming.TabIndex = 4;
+            this.mianzeshengming.UseVisualStyleBackColor = true;
+            this.mianzeshengming.CheckedChanged += new System.EventHandler(this.mianzeshengming_CheckedChanged);
+            this.mianzeshengming.CheckStateChanged += new System.EventHandler(this.mianzeshengming_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.linkLabel1.Location = new System.Drawing.Point(379, 43);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(65, 20);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "免责声明";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // fangfa
+            // 
+            this.fangfa.AutoSize = true;
+            this.fangfa.Location = new System.Drawing.Point(12, 156);
+            this.fangfa.Name = "fangfa";
+            this.fangfa.Size = new System.Drawing.Size(219, 80);
+            this.fangfa.TabIndex = 6;
+            this.fangfa.Text = "使用方法：先复制文字或图片，\r\n同意免责声明后点击”开团“按钮\r\n再点击微信的聊天栏\r\n即可实现自动发送";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label6.Location = new System.Drawing.Point(16, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 27);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "间隔时间:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(117, 49);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(75, 25);
+            this.numericUpDown3.TabIndex = 12;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(200, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "毫秒";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(117, 83);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(77, 25);
+            this.numericUpDown2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label3.Location = new System.Drawing.Point(16, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 27);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "延时启动:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "秒";
+            // 
+            // kaituan
+            // 
+            this.kaituan.Enabled = false;
+            this.kaituan.Location = new System.Drawing.Point(322, 66);
+            this.kaituan.Name = "kaituan";
+            this.kaituan.Size = new System.Drawing.Size(131, 70);
+            this.kaituan.TabIndex = 19;
+            this.kaituan.Text = "开团！！！";
+            this.kaituan.UseVisualStyleBackColor = true;
+            this.kaituan.Click += new System.EventHandler(this.kaituan_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(322, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 44);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "双击急停";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this.button1.MouseCaptureChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Enabled = false;
+            this.numericUpDown4.Location = new System.Drawing.Point(117, 115);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(77, 25);
+            this.numericUpDown4.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label5.Location = new System.Drawing.Point(16, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 27);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "当前是:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(200, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "次";
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(404, 202);
+            this.num5.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.num5.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(49, 25);
+            this.num5.TabIndex = 24;
+            this.num5.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(305, 204);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 20);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "使用线程数：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(465, 251);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.kaituan);
+            this.Controls.Add(this.mianzeshengming);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fangfa);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "一键开团工具 by:冷面小粉龙";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox mianzeshengming;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label fangfa;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button kaituan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown num5;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ One-clickGroupOpeningToolkit/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace One_clickGroupOpeningToolkit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://maze778.github.io/disclaimer.html");
+        }
+
+        private bool stopLoop = false;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mianzeshengming_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mianzeshengming.Checked == true)
+            {
+                kaituan.Enabled = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                kaituan.Enabled = false;
+                button1.Enabled = false;
+            }
+        }
+        bool on = false;
+        private async void kaituan_Click(object sender, EventArgs e)
+        {
+            int jiange = (int)numericUpDown3.Value;
+            mianzeshengming.Enabled = false;
+            double cishu = (double)numericUpDown1.Value;
+            int yanshi = ((int)numericUpDown2.Value) * 1000;
+            int xianchengnum = (int)num5.Value;
+            double result = Math.Round(cishu / xianchengnum);
+            on = true;
+            numericUpDown4.Value = 0;
+
+            await Task.Delay(yanshi);
+            for (int i = 0; i < xianchengnum; i++)
+            {
+                if (on)
+                {
+                    await Task.Run(async () =>
+                    {
+                        for (int j = 0; j < (int)result; j++)
+                        {
+                            this.Invoke(new MethodInvoker(() => SendKeys.SendWait("^v")));
+                            this.Invoke(new MethodInvoker(() => SendKeys.SendWait("{ENTER}")));
+                            this.Invoke(new MethodInvoker(() => numericUpDown4.Value += 1));
+                            await Task.Delay(jiange);
+                        }
+                    });
+                }
+            }
+            mianzeshengming.Enabled = true;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (on)
+            {
+                on = false;
+            }
+            mianzeshengming.Enabled = true;
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            while (on)
+            {
+                on = false;
+            }
+        }
+    }
+}
